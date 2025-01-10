9ec90faca5a0327ccea98d5d4f87d27a057db6db
yunumu <1416900346@qq.com>
2025-01-10 16:15:19 +0800

    refactor(js): 重构 JavaScript 代码并添加闭包相关示例
    - 删除可能导致内存问题的代码
    - 添加经典闭包示例，展示如何使用闭包实现计数器
    - 增加数据私有化示例，演示如何使用闭包保护私有数据
    - 引入闭包工厂概念，提供乘法函数生成示例

--- js/闭包.js
+++ js/闭包.js
@@ -1,8 +1,43 @@
-// 可能导致内存问题的例子
-function createLargeData() {
-  const largeData = new Array(10000)
+// 经典闭包
+function createCounter() {
+  let count = 0
 
-  return function () {
-    return largeData // largeData 一直被引用
+  return () => {
+    count++
+    return count
   }
 }
+
+const returnFn = createCounter()
+
+console.log(returnFn())
+console.log(returnFn())
+console.log(returnFn())
+
+// 数据私有化
+function createUser() {
+  let password = '123456'
+
+  return {
+    getPassword: () => password,
+    checkPassword: (inputPassword) => inputPassword === password,
+  }
+}
+
+const user = createUser()
+
+console.log(user.getPassword())
+console.log(user.checkPassword('1'))
+
+// 闭包工厂
+function multiply(a) {
+  return (b) => {
+    return a * b
+  }
+}
+
+const multiplyFn1 = multiply(2)
+const multiplyFn2 = multiply(3)
+
+console.log(multiplyFn1(2))
+console.log(multiplyFn2(3))
